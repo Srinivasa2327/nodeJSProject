b255e7d4091f4896a55acbc26cf129f02b2dc285
Srinivasa Jagu <srinivasa.jagu@gmail.com>
2021-08-11 21:21:33 +0000

    changes to index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// importing azure storage blob module
 const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
 var parse = require('papaparse');
 let downloaded
@@ -5,26 +6,18 @@ let lastUpdateTime
 
 async function main() {
 
-// Enter your storage account name and shared key
-//const account = "srinistorage77";
-
 const account = "inlinemarketdata";
-//const accountKey = "Rxruces1v7xH7ymJUouEZdYpAaOg//Do5oN5UtE7z48kSIJ0kwah8i9s7TGuS3ztRS1P8RbZu2Mmi6wXk5mM0g==";
-
 const accountKey = "jbEyQqsONKXxZU0oOB/mXoSQU16y3vP3RG3GWqCjw42+pXuIcH/xDxqvo3b/mZRFn0dc1uxnBIba5MVowjPvww==";
 
-// Use SharedKeyCredential with storage account and account key
-// SharedKeyCredential is only avaiable in Node.js runtime, not in browsers
 const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 const blobServiceClient = new BlobServiceClient(
   `https://${account}.blob.core.windows.net`,
   sharedKeyCredential
 );
 
-
 const containerClient = blobServiceClient.getContainerClient("customerdata");
 
-
+// Get the details of last modified time of the data file
 for await (const item of containerClient.listBlobsByHierarchy("/")) {
   if (item.name === "us-500.csv") {
     //console.log(`\tBlobItem: name - ${item.name}, last modified - ${item.properties.lastModified}`);
@@ -34,16 +27,8 @@ for await (const item of containerClient.listBlobsByHierarchy("/")) {
 
 const blobClient = containerClient.getBlobClient("us-500.csv");
 
-// Get blob content from position 0 to the end
-// In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
 const downloadBlockBlobResponse = await blobClient.download();
 downloaded = await streamToString(downloadBlockBlobResponse.readableStreamBody);
-
-//const downloadBlockBlobResponse = blobClient.download();
-//const downloaded = streamToString(downloadBlockBlobResponse.readableStreamBody);
-
-//console.log("+++++++++++++++++++++++++++++++++++++++++++", downloaded,"+++++++++++++++++++++++++++++++++++++");
-
 
 // [Node.js only] A helper method used to read a Node.js readable stream into string
 async function streamToString(readableStream) {
@@ -59,10 +44,7 @@ async function streamToString(readableStream) {
   });
 }
 
-
-
-
-}
+}  // end of main function
 
 main()
   .then(
@@ -75,32 +57,23 @@ var url = require('url');
 
 // Create a server
 http.createServer( function (request, response) {  
-/*
-response.writeHead(200, {'Content-Type': 'text/html'});
 
-response.end(downloaded);
-*/
- 
- response.writeHead(200, { 'Content-Type': 'text/html' });
+response.writeHead(200, { 'Content-Type': 'text/html' });
 
-        var html = createTable();
+//invoke html content creation function
+var html = createTable();
 
-        response.end(html);
+response.end(html);
 
- 
-//}).listen(8082);
 }).listen(process.env.PORT||8080);
-// Console will print the message
 console.log('Server running at http://127.0.0.1:8080/');
 	  
-
-  }
+	}
   
   )
  
 
- 
- 
+// Function that converts raw csv data into html table content
  function createTable() {
   var results = parse.parse(downloaded, {
     delimiter: ",",
